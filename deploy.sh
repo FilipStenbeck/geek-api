4af457f5e6cd62ccf76db2562d182729b51704d5
Filip Stenbeck <filip.stenbeck@tre.se>
2019-06-30 17:08:01 +0200

    fix: add docker support in deploy step

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,9 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
-echo $PACKAGE_VERSION
-echo $DOCKER_HUB_USER
 PACKAGE_VERSION="$(echo -e "${PACKAGE_VERSION}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 
-echo geek-api:latest filipstenbeck/geek-api:$PACKAGE_VERSION
+echo Deploying: geek-api:latest filipstenbeck/geek-api:$PACKAGE_VERSION
 docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD
 
 docker tag geek-api:latest filipstenbeck/geek-api:$PACKAGE_VERSION
